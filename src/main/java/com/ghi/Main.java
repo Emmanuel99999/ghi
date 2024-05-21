b73959f33b13d0d89e5079ae935bfd49309d848d
canbeagle <eafcanbeagle@gmail.com>
2024-05-21 18:42:13 -0500

    atributos añadidos para clases principales

--- src/main/java/com/ghi/Main.java
+++ src/main/java/com/ghi/Main.java
@@ -1,7 +1,33 @@
 package com.ghi;
-
+import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+public class Person{
+
+private String name= "Blank";
+private String lastName;
+private String emailAdress;
+private Date dateOfBirth;
+private int id;
+private char gender;
+
+}
+class Customer extends Person {
+    protected boolean advertise;
+    protected boolean news;
+    protected boolean discounts;
+    protected boolean promos;
+    protected boolean referralProgram;
+
+}
+class Employee extends Person{
+    protected String password;
+    protected int jobId;
+    protected float wage;
+    protected String title;
+    protected Date startingDate;
+    protected boolean accountStatus;
+}
 }
