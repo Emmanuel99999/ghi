d0765007ec270f37a3250a6997e1893042d6ba5c
canbeagle <eafcanbeagle@gmail.com>
2024-05-21 19:05:22 -0500

    get set añadido para clase padre persona

--- src/main/java/com/ghi/Main.java
+++ src/main/java/com/ghi/Main.java
@@ -1,17 +1,60 @@
 package com.ghi;
 import java.util.Date;
+
+import com.ghi.Main.Person;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-public class Person{
+abstract class Person{
 
-private String name= "Blank";
-private String lastName;
-private String emailAdress;
-private Date dateOfBirth;
-private int id;
-private char gender;
+protected String name= "Blank";
+protected String lastName;
+protected String emailAddress;
+protected Date dateOfBirth;
+protected int id;
+protected char gender;
+
+//getters
+public String getName() {
+    return name;
+}
+
+public String getLastName() {
+    return lastName;
+}
+
+public String getEmailAddress() {
+    return emailAddress;
+}
+public Date getDateOfBirth(){
+    return dateOfBirth;
+}
+public int getId(){
+    return id;
+}
+public char getGender(){
+    return gender;
+}
+//setters
+public void setName(String name) {
+    this.name = name;
+}
+public void setLastName(String lastName) {
+    this.lastName = lastName;
+}
+public void setEmailAddress(String emailAddress){
+    this.emailAddress = emailAddress;
+}
+public void setDateOfBirth(Date dateOfBirth){
+    this.dateOfBirth = dateOfBirth;
+}
+public void setId(int id){
+    this.id = id;
+}
+public void setChar(char gender){
+    this.gender = gender;
+}
 
 }
 class Customer extends Person {
